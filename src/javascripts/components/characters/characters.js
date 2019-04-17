a4789e5f3817d26bd93b6d6a9a35e2b24c836d22
Heath JH Moore <heathjhmoore@gmail.com>
2019-04-16 19:42:58 -0500

    built out XHR request via axios

---
+++ src/javascripts/components/characters/characters.js
@@ -0,0 +1,34 @@
+import characterData from '../../helpers/data/characterData';
+
+import util from '../../helpers/util';
+
+let characters = [];
+
+const domStringBuilder = () => {
+  let domString = '';
+  characters.forEach((char) => {
+    domString += '<div>';
+    domString += '<div class="img-holder">';
+    domString += `<img src=${char.imageUrl} />`;
+    domString += '</div>';
+    domString += `<h2 class="char-name">${char.name}</h2>`;
+    domString += '</div>';
+  });
+  util.printToDom('characters', domString);
+};
+
+const getData = () => {
+  characterData.getCharacterData()
+    // response is the name of the variable that will catch what you want from the request;
+    // then is what runs in case of success, catch runs in case of error;
+    .then((response) => {
+      const charactersArray = response.data.characters;
+      characters = charactersArray;
+      domStringBuilder();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export default { getData };
